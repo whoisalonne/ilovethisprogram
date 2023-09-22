8aefffe91a01c86f747e4b2332110f762c464234
Janderson Lima <backupaw197@gmail.com>
2023-09-21 22:42:55 -0300

    Arrays dos temas light/dark e let/var

--- javascript.js
+++ javascript.js
@@ -1,52 +1,54 @@
-var criador = window.document.getElementById("webmaster");
+let criador = window.document.getElementById("webmaster");
 
 
 criador.addEventListener('mouseover', mudar);
 function mudar() { criador.textContent = "Alonne" };
 
-var titulo = document.getElementById('titulo');
-var claro = document.getElementById('dark');
-var loveTxt = document.getElementById('love-txt');
-var blogTxt = document.getElementById('blog-txt');
-var sup = document.getElementById('suptitulo');
-var sub = document.getElementById('subtitulo');
-var amor = document.getElementById('amor');
-var main = document.getElementById('frontend');
-var nome = document.getElementById('nome');
-var corpo = document.body;
-var cabeca = document.getElementById('cabeca');
+let titulo = document.getElementById('titulo');
+let claro = document.getElementById('dark');
+let loveTxt = document.getElementById('love-txt');
+let blogTxt = document.getElementById('blog-txt');
+let sup = document.getElementById('suptitulo');
+let sub = document.getElementById('subtitulo');
+let amor = document.getElementById('amor');
+let main = document.getElementById('frontend');
+let nome = document.getElementById('nome');
+let corpo = document.body;
+let cabeca = document.getElementById('cabeca');
+
+let colorWhite = [loveTxt, blogTxt, titulo, ul];
+let bgBlack = [main, cabeca, corpo];
 
 claro.addEventListener('click', dark);
 
-function dark() { 
-    if (main.style.backgroundColor == "white") { 
-    
-      loveTxt.style.color = "white";
-      blogTxt.style.color = "white";
+function dark() {
+  if (main.style.backgroundColor == "white") {
+
+    // Percorre os arrays e aplica estilos
+    for (var i = 0; i < colorWhite.length; i++) {
+      colorWhite[i].style.color = 'white';
+      bgBlack[i].style.backgroundColor = 'black';
+
+      // Mudanças adicionais de estilo
       sup.style.color = "#616161";
       amor.style.color = "#6746E9";
       nome.style.color = "#b2b2b2";
       sub.style.color = "#616161";
-      main.style.backgroundColor = "#040404";
-      cabeca.style.backgroundColor = "#000000";
-      ul.style.color = "white";
-      titulo.style.color = "white"; 
       claro.textContent = "Light";
-      corpo.style.backgroundColor = "black";
+    }
+  } else {
 
-    } else { 
-      loveTxt.style.color = "black";
-      blogTxt.style.color = "black";
+    // Percorre os arrays e aplica estilos
+    for (var i = 0; i < colorWhite.length; i++) {
+      colorWhite[i].style.color = 'black';
+      bgBlack[i].style.backgroundColor = 'white';
+
+      // Mudanças adicionais de estilo
       sup.style.color = "#b2b2b2";
       amor.style.color = "#E21F14";
       nome.style.color = "#616161";
       sub.style.color = "#616161";
-      main.style.backgroundColor = "white";
-      cabeca.style.backgroundColor = "white";
-      ul.style.color = "black";
-      titulo.style.color = "black"; 
       claro.textContent = "Dark";
-      corpo.style.backgroundColor = "";
     }
   }
-  
+}
