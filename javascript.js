adbf17f207915547d33a910569a6e523eae87170
Janderson Lima <backupaw197@gmail.com>
2023-10-01 01:45:55 -0300

    Adição da segunda página com mediaquery não finalizado

--- javascript.js
+++ javascript.js
@@ -1,8 +1,3 @@
-let criador = window.document.getElementById("webmaster");
-
-
-criador.addEventListener('mouseover', mudar);
-function mudar() { criador.textContent = "Alonne" };
 
 let titulo = document.getElementById('titulo');
 let claro = document.getElementById('dark');
@@ -16,18 +11,39 @@ let nome = document.getElementById('nome');
 let corpo = document.body;
 let cabeca = document.getElementById('cabeca');
 let ul = document.getElementById('lista-cabeca');
+let link = document.getElementById('link')
+let paragrafos = document.querySelectorAll('p');
+let bgYborder = document.getElementById('bgYellowPlus');
+let bgYellow = document.querySelectorAll('.bgYellow');
+let imgContent = document.getElementById('img');
 
-let colorWhite = [loveTxt, blogTxt, titulo, ul];
+
+let pArray = Array.from(paragrafos);
+let bgYArray = Array.from(bgYellow);
+
+let colorWhite = [loveTxt, blogTxt, titulo, ul, link];
 let bgBlack = [main, cabeca, corpo];
 
 claro.addEventListener('click', dark);
 
+// Função dark e códigos de transição em arrays
+
 function dark() {
   if (main.style.backgroundColor == "white") {
 
-    // Percorre os arrays e aplica estilos
+    // Percorre os arrays e aplica estilos - Esuro
     for (var i = 0; i <= colorWhite.length - 1; i++) {
       colorWhite[i].style.color = 'white';
+    }
+
+    for (var i = 0; i <= pArray.length - 1; i++) {
+      pArray[i].style.color = 'white'
+    }
+
+    for (var i = 0; i <= bgYArray.length - 1; i++) {
+      bgYArray[i].style.backgroundColor = '#8b9aff';
+    }
+    for (var i = 0; i <= bgBlack.length - 1; i++) {
       bgBlack[i].style.backgroundColor = 'black';
 
       // Mudanças adicionais de estilo
@@ -35,21 +51,40 @@ function dark() {
       amor.style.color = "#6746E9";
       nome.style.color = "#b2b2b2";
       sub.style.color = "#616161";
+      imgContent.style.borderColor = 'white';
+
+      bgYborder.style.backgroundColor = '#8b9aff'
       claro.textContent = "Light";
     }
   } else {
 
     // Percorre os arrays e aplica estilos
     for (var i = 0; i <= colorWhite.length - 1; i++) {
+
       colorWhite[i].style.color = 'black';
+    }
+    for (var i = 0; i <= pArray.length - 1; i++) {
+      pArray[i].style.color = 'black'
+    }
+    for (var i = 0; i <= bgBlack.length - 1; i++) {
       bgBlack[i].style.backgroundColor = 'white';
+    }
+
+    for (var i = 0; i <= bgYArray.length - 1; i++) {
+      bgYArray[i].style.color = 'black';
+      bgYArray[i].style.backgroundColor = 'yellow';
 
       // Mudanças adicionais de estilo
       sup.style.color = "#b2b2b2";
       amor.style.color = "#E21F14";
       nome.style.color = "#616161";
       sub.style.color = "#616161";
+      bgYborder.style.color = 'black';
+
+      imgContent.style.borderColor = 'black';
+      bgYborder.style.backgroundColor = 'yellow'
       claro.textContent = "Dark";
     }
   }
 }
+
